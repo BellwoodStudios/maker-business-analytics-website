67f226fa3cbefcc358458101f5e851356f5086ad
Sam MacPherson <xylom44@gmail.com>
2020-05-08 15:06:49 -0400

    parse block number to int for integer sorting instead of string sorting which broke after block 10M

--- src/api/model/Block.js
+++ src/api/model/Block.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 export default class Block {
 
     constructor (data) {
-        this.number = data.blockNumber;
+        this.number = parseInt(data.blockNumber);
         this.timestamp = moment.unix(data.blockTimestamp);
     }
 
